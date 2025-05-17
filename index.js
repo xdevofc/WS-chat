74214416537e4e67215f6b86746b79e136177585
xdev <xdevofc@gmail.com>
2025-05-17 14:26:47 -0300

    separating in different files for more organization

---
+++ index.js
@@ -0,0 +1,42 @@
+let ws;
+
+function connect(){
+
+
+    ws = new WebSocket("ws://localhost:8080/ws");
+
+    ws.onopen = function (){
+        
+        console.log("connected to the Websocket server");
+    }
+
+    ws.onmessage = function(event) {
+
+        console.log(JSON.parse(event.data))
+        const {username, message} = JSON.parse(event.data) 
+        let messageDisplay = document.getElementById("message");
+        messageDisplay.innerHTML += `<p>USER:${username} says: ${message}</p>`;
+    }
+
+    ws.onclose = function () {
+        console.log("WS connection closed... Retrying")
+        setTimeout(connect, 1000)
+    }
+
+    ws.onerror = function (error){
+        console.error(error)
+    }
+
+} 
+function sendMessage(){
+        let input = document.getElementById("messageInput");
+        let message = input.value;
+        input.value = "";
+        let user = document.getElementById("userInput");
+        let username = user.value;
+        user.value = "";
+
+        const payload = JSON.stringify({username: username, message: message})
+        ws.send(payload)
+    }
+connect()
